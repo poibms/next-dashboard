edda9ff340991bc681c2b536ddc3a335552064d1
Maksim Balantsevich <M.Balantsevich@gmail.onmicrosoft.com>
2024-12-02 17:22:56 +0300

    Chapter 13

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -145,10 +145,8 @@ export async function fetchInvoiceById(id: string) {
 
     const invoice = data.rows.map((invoice) => ({
       ...invoice,
-      // Convert amount from cents to dollars
       amount: invoice.amount / 100,
     }));
-
     return invoice[0];
   } catch (error) {
     console.error('Database Error:', error);
